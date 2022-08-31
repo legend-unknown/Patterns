d0fc9b4d2517579553bdeaa8342d7228b512c768
legend <unknown11989198@gmail.com>
2022-08-31 14:17:33 +0530

    first version

---
+++ butterfly.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class butterfly{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j<=i)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            for(int j=1;j<=n;j++)
+            {
+                if(j>=n-i)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j<=i)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            for(int j=1;j<=n;j++)
+            {
+                if(j>=n-i)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
